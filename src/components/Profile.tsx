95ad3394c13e1aea833b28c739553340f32c86de
viniciusbe <vinicius.bernardesds@gmail.com>
2021-02-23 15:57:57 -0300

    created next app

---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/viniciusbe.png" alt="Vinicius Bernardes" />
+            <div>
+                <strong>Vinícius Bernardes</strong>
+
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level 1
+                    </p>
+            </div>
+        </div>
+    )
+}
